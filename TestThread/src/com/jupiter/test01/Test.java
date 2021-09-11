18d2d150116ae67d97d3669878ac32e52afdd127
louyecong <luoyecong@sdbattery.com>
2021-09-11 23:39:26 +0800

    feat: 线程名字

--- TestThread/src/com/jupiter/test01/Test.java
+++ TestThread/src/com/jupiter/test01/Test.java
@@ -6,18 +6,19 @@ package com.jupiter.test01;
 public class Test {
     public static void main(String[] args) {
         // 主线程中，也要输出10个数字
+        Thread.currentThread().setName("主线程");
 
         for (int i = 0; i <= 100; i++) {
-            System.out.println("main1-----------" + i);
+            System.out.println(Thread.currentThread().getName() + "1" + i);
         }
         // 制造其他线程，和主线程争抢资源
-        TestThread tt = new TestThread();
+        TestThread tt = new TestThread("子线程");
 //        tt.run(); // 调用run方法，执行线程中的任务 run方法不能直接调用，直接调用就会被当做一个普通的方法；
         // tt起作用要调用父类 start方法
         tt.start();
 
         for (int i = 0; i <= 100; i++) {
-            System.out.println("main2-----------" + i);
+            System.out.println(Thread.currentThread().getName() + "2" + i);
         }
     }
 }
--- TestThread/src/com/jupiter/test01/TestThread.java
+++ TestThread/src/com/jupiter/test01/TestThread.java
@@ -5,6 +5,9 @@ package com.jupiter.test01;
  * 继承Thread类
  */
 public class TestThread extends Thread {
+    public TestThread(String name) {
+        super(name);
+    }
 
     /**
      * 线程对象争抢资源，线程需要执行的任务是什么
@@ -12,7 +15,7 @@ public class TestThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i <= 100; i++) {
-            System.out.println(i);
+            System.out.println(this.getName() + i);
         }
     }
 }
